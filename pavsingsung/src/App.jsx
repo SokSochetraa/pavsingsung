c1369c408301ac3844ae06924b3ecc4680fb882c
សុខ​ សុចិត្រា - Sok Sochetra <sok.sochetra.2822@rupp.edu.kh>
2025-04-12 23:28:26 +0700

    Tos Lg Pav Sing Sung!!

--- pavsingsung/src/App.jsx
+++ pavsingsung/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
 
   const play = (choice) => {
     const computer = options[Math.floor(Math.random() * options.length)];
-
     setPlayerChoice("rock");
     setComputerChoice("rockCom");
     setResult("");
@@ -39,7 +38,6 @@ function App() {
 
   const getResult = (player, computer) => {
     if (player === computer) return "Draw!";
-
     if (
       (player === "rock" && computer === "scissors") ||
       (player === "paper" && computer === "rock") ||
@@ -51,18 +49,18 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-8 p-8">
-      <h2 className="text-center m-[35px] font-bungee text-7xl md:text-6xl">
+    <div className="min-h-screen flex flex-col items-center justify-center md:gap-8 mt-2">
+      <h2 className="text-center mb-4 font-bungee text-5xl md:text-7xl text-xl">
         Lg Pav Sing Sung
       </h2>
 
-      <div className="flex flex-col md:flex-row w-full max-w-5xl justify-between items-center gap-8 px-8">
+      <div className="flex flex-col md:flex-row w-full max-w-5xl justify-between items-center gap-8 px-2">
         <div className="text-center flex flex-col items-center">
-          <h4 className="font-bungee text-3xl mb-4">Player</h4>
-          <div className="w-[300px] h-[200px] flex justify-center items-center bg-[#1e1e1e] rounded-xl shadow">
+          <h4 className="font-bungee text-2xl md:text-3xl md:mb-4">Player</h4>
+          <div className="w-[250px] max-w-xs h-[150px] md:h-[200px] flex justify-center items-center bg-[#1e1e1e] rounded-xl shadow">
             {playerChoice && (
               <img
-                className={`w-[150px] transform rotate-90 transition-transform duration-300 ${
+                className={`w-[100px] md:w-[150px] transform rotate-90 transition-transform duration-300 ${
                   isRevealing ? "animate-shake" : ""
                 }`}
                 src={images[playerChoice]}
@@ -71,15 +69,17 @@ function App() {
             )}
           </div>
         </div>
-        <h4 className="font-bungee text-white text-5xl mb-1">
+
+        <h4 className="w-full h-[58px] text-center font-bungee text-white text-4xl md:text-5xl">
           {!isRevealing && result}
         </h4>
-        <div className="text-center flex flex-col items-center">
-          <h4 className="font-bungee text-3xl mb-4">Computer</h4>
-          <div className="w-[300px] h-[200px] flex justify-center items-center bg-[#1e1e1e] rounded-xl shadow">
+
+        <div className="text-center flex flex-col-reverse md:flex-col items-center">
+          <h4 className="font-bungee text-2xl md:text-3xl mb-4">Computer</h4>
+          <div className="w-[250px] max-w-xs h-[150px] md:h-[200px] flex justify-center items-center bg-[#1e1e1e] rounded-xl shadow">
             {computerChoice && (
               <img
-                className={`w-[150px] transform rotate-90 scale-y-[-1] transition-transform duration-300 ${
+                className={`w-[100px] md:w-[150px] transform rotate-90 scale-y-[-1] transition-transform duration-300 ${
                   isRevealing ? "animate-shake" : ""
                 }`}
                 src={images[computerChoice]}
@@ -90,25 +90,27 @@ function App() {
         </div>
       </div>
 
-      <div className="flex gap-4 mt-8">
+      <div className="flex flex-wrap justify-center gap-4">
         {options.map((option) => (
           <button
             key={option}
             onClick={() => play(option)}
-            className="w-[150px] h-[100px] bg-[#1e1e1e] rounded-lg shadow-md border-[2.5px] border-transparent hover:border-[#cd1401] hover:scale-105 transition transform flex items-center justify-center disabled:opacity-50"
+            className="w-[100px] h-[80px] md:w-[150px] md:h-[100px] bg-[#1e1e1e] rounded-lg shadow-md border-[2.5px] border-transparent hover:border-[#cd1401] hover:scale-105 transition transform flex items-center justify-center disabled:opacity-50"
             disabled={isRevealing}
           >
             <img
               src={images[option]}
               alt={option}
-              className="w-[40px] rotate-90"
+              className="w-[30px] md:w-[40px] rotate-90"
             />
           </button>
         ))}
       </div>
 
-      <div className="w-full flex flex-col items-center justify-center">
-        <h4 className="font-bungee text-xl text-gray-600">Score: {point}</h4>
+      <div className="w-full flex flex-col items-center justify-center mt-2">
+        <h4 className="font-bungee text-lg md:text-xl text-gray-600">
+          Score: {point}
+        </h4>
       </div>
     </div>
   );
